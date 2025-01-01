081768932e84635d5944419815b07923cb28b43f
Pranav Kumar <kumar2pranav@gmail.com>
2025-01-02 03:49:49 +0530

    Add DataType.java in 1.Basics folder

---
+++ 1.Basics/DataType.java
@@ -0,0 +1,11 @@
+public class DataType{
+    public static void main(String[] args){
+        char c='a';
+
+        System.out.println(c+3);
+
+        int i=c+10;
+        System.out.println(i);
+
+    }
+}
