b5d626ff09d1efbe43ab7f1405f98955abc54232
Pranav Kumar <kumar2pranav@gmail.com>
2025-01-03 02:30:04 +0530

    Auto commit: Fri Jan  3 02:30:04 IST 2025

---
+++ 1.Basics/Operators.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Operators{
+    public static void main(String[] args){
+        System.out.println(9+5); //addition
+        System.out.println(9-5); //sub
+        System.out.println(9*5); //Mul
+        System.out.println(9/5); //Div
+        System.out.println(9.0/5);
+        System.out.println(9%5); 
+
+
+    }
+}
