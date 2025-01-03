f2491efbacfebc59d2741a49e512ba05e2f04628
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-04 02:33:19 +0530

    Added new file Patterns7.java in folder 2.Patterns

---
+++ 2.Patterns/Pattern7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern7{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the value of N: ");
+        int n=sc.nextInt();
+        int counter=1;
+        for(int i=1;i<=n;i++){
+            
+            for(int j=1;j<=i;j++){
+                
+                System.out.print(counter);
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+}
