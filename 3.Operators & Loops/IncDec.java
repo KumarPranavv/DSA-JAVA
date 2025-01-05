ffcceecf0747c49177a62783464b1492c1397e18
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-05 22:54:52 +0530

    Added a new file IncDec.java

---
+++ 3.Operators & Loops/IncDec.java
@@ -0,0 +1,22 @@
+public class IncDec{
+    public static void main(String[] args){
+        int a=10;
+        System.out.println(a++);
+
+        a++;
+        ++a;
+        System.out.println(a);
+        int b=a++;
+        System.out.println(a);
+        System.out.println(b);
+        int c=10;
+        c*=2; // c=c*2;
+        System.out.println(c);
+        int ans=2*3/2;
+        System.out.println(ans);
+
+
+
+
+    }
+}
