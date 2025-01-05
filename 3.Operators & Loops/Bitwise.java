968f454ea7d24632aed8e31ac8696fcf9c3d69c0
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-05 22:02:30 +0530

    Added new folder 3.Operators and a file Operators.java

---
+++ 3.Operators & Loops/Bitwise.java
@@ -0,0 +1,12 @@
+public class Bitwise{
+    public static void main(String[] args){
+        System.out.println(2 & 3); //AND
+        System.out.println(2 | 3); //OR
+        System.out.println(2 ^ 3);  //XOR
+        System.out.println(~2); //NEGATION
+        System.out.println(34 << 1); //LEFT-SHIFT
+        System.out.println(34 >> 2); //RIGHT-SHIFT
+        System.out.println(-4 << 1); 
+        System.out.println(-4 >> 1);
+    }
+}
