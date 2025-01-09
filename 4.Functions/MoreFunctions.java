52594c61ef10af52000991e461805edc7cf5f511
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-10 00:58:46 +0530

    Added new file

--- 4.Functions/MoreFunctions.java
+++ 4.Functions/MoreFunctions.java
@@ -3,10 +3,21 @@ public class MoreFunctions{
         double ans=a+b+c;
         return ans;
     }
+
+    public static void printEven(int start,int end){
+        for(int i=start;i<=end;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+
+
+    }
     public static void main(String[] args){
-        int num1=10;
+        printEven(1,160);
+        /*int num1=10;
         int num2=90;
-        double result=sum(num1,num2,1.2);
-        System.out.println("the sum is: "+result);
+        double result=sum(num1,num2,1.2);*/
+        //System.out.println("the sum is: "+sum(10,88,1.99));
     }
 }
