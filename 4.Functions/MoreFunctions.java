493189c9e418bcec5ef34b55e91406073d766420
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-09 23:53:55 +0530

    Added new file

---
+++ 4.Functions/MoreFunctions.java
@@ -0,0 +1,12 @@
+public class MoreFunctions{
+    public static int sum(int a,int b){
+        int ans=a+b;
+        return ans;
+    }
+    public static void main(String[] args){
+        int num1=10;
+        int num2=90;
+        int result=sum(num1,num2);
+        System.out.println("the sum is: "+result);
+    }
+}
