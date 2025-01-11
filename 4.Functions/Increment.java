3298d32cfb726810d8e760f344fa23e61b274382
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-11 20:10:29 +0530

    Added new file

---
+++ 4.Functions/Increment.java
@@ -0,0 +1,10 @@
+public class Increment{
+    public static void increment(int n){
+        n++;
+    }
+    public static void main(String[] args){
+        int a=10;
+        increment(a);
+        System.out.println(a);
+    }
+}
