c735ab8da770b9520deceb8cd71b95944adbc60b
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-13 23:56:41 +0530

    Added new file

---
+++ 5.Arrays/Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[1]=4;
+        arr = new int[2];
+        System.out.println(arr[0]);
+    }
+}
