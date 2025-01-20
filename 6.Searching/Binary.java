595ea5bff921a92f3fa9d19a20915d38d4d9fe4a
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-21 02:14:44 +0530

    Added new file

---
+++ 6.Searching/Binary.java
@@ -0,0 +1,30 @@
+public class Binary{
+    public static int binarySearch(int[] arr, int x){
+        int start=0;
+        int end=arr.length-1;
+        int mid=start;
+
+        while(start<=end){
+        mid=start+(end-start)/2;
+            if(arr[mid]>x){
+                end=mid-1;
+            }
+            else if(arr[mid]<x){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+    
+    
+
+
+}
