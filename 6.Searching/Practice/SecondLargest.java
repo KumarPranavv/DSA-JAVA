c16dec76015b960c98142faa76d4ab5f7b119d50
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-26 01:58:45 +0530

    added new file

---
+++ 6.Searching/Practice/SecondLargest.java
@@ -0,0 +1,25 @@
+public class SecondLargest{
+    public static int second(int[] arr){
+        if(arr.length==0){
+            return Integer.MIN_VALUE;
+        }
+
+        int largest=arr[0];
+        int secondLarg=Integer.MIN_VALUE;
+        for(int i=1;i<arr.length;i++){
+            if(largest<arr[i]){
+                secondLarg=largest;
+                largest=arr[i];
+            }else if(secondLarg<arr[i] && arr[i]!=largest){
+                secondLarg=arr[i];
+            }
+        }
+        return secondLarg;
+
+    }
+    public static void main(String[] args){
+        int[] arr={4,7,13,76,25,9,8,19};
+        int ans=second(arr);
+        System.out.println("Second largest element is: "+ans);
+    }
+}
