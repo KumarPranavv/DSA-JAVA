d50783c9a8e5531e3cacac9273322d1e9dc0427b
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-28 12:24:22 +0530

    Updated a file

--- 7. Strings/StringUse.java
+++ 7. Strings/StringUse.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 public class StringUse {
+    public static void print(String s){
+        for(int i=0;i<s.length();i++){
+            System.out.println(s.charAt(i));
+
+        }
+    }
     public static void main(String[] args) {
         char c='c';
         String str="abcdefgh";
-        System.out.println(str.charAt(1));
+        print(str);
+        /*System.out.println(str.charAt(1));
         System.out.println("Length of String: "+str.length());
         System.out.println("Substring: "+str.substring(1,6)); //end index is not included in this 
         System.out.println(str.substring(3)); // begin/start print all from begin index
         System.out.println(str.substring(0)); // similar as print(str)
         System.out.println(str.substring(str.length())); //empty string
-        System.out.println(str.contains("cdf"));
+        System.out.println(str.contains("cdf")); */
     }
 }
