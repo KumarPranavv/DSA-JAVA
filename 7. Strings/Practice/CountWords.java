64eeda94003de986a1c11bab6431d6207b4c2a3e
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-28 14:01:36 +0530

    Added new file

--- 7. Strings/Practice/CountWords.java
+++ 7. Strings/Practice/CountWords.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class CountWords {
-    public static int count(String s){
+    public static void count(String s){
         int c=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==' '){
                 c++;
             }
         }
-        return c;
+        System.out.println("The number of spaces: "+ c);
     }
     public static void main(String[] args) {
         String str="Hola Amigos, kaise ho theek ho";
-        System.out.println(count(str));
+        count(str);
 
     }
 }
