e8f7e2abf283d1ab0e4692be2c94707710ac0402
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-28 12:10:14 +0530

    Added new file

---
+++ 7. Strings/StringUse.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class StringUse {
+    public static void main(String[] args) {
+        char c='c';
+        String str="abcdefgh";
+        System.out.println(str.charAt(1));
+
+    }
+}
