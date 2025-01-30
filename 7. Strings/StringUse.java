d7d0349d31ff6a9be4fcf760c9c0082197e4c03d
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-01-30 22:29:39 +0530

    Updated a File

--- 7. Strings/StringUse.java
+++ 7. Strings/StringUse.java
@@ -7,7 +7,22 @@ public class StringUse {
         }
     }
     public static void main(String[] args) {
-        char c='c';
+        int[] arr1={1,2,3};
+        //int[] arr2={1,2,3}; now
+        int[] arr2=arr1;
+        if(arr1==arr2){ //refers the address of arr1 and arr2 , that must be different 
+            System.out.println("equal");
+            
+        }else{
+            System.out.println("Not equal");
+
+        }
+        System.out.println(arr1);
+        System.out.println(arr2);
+
+
+
+       /* char c='c';
         String str="abcdefgh";
         str="abc";
         
