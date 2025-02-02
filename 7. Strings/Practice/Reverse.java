fc6e893f40ad3c2775077ab3c9a6e42d30a0d742
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-02 22:42:30 +0530

    Added new file

---
+++ 7. Strings/Practice/Reverse.java
@@ -0,0 +1,22 @@
+public class Reverse{
+    public static String reverseString(String s){
+        String result="";
+        int i=s.length()-1;
+        int end=s.length();
+        while(i>=0){
+            if(s.charAt(i)==' '){
+               result+= s.substring(i+1,end)+" ";
+               end=i;
+            }
+            i--;
+        }
+        return result;
+
+
+    }
+    public static void main(String[] args){
+        String str="Hola AMigos, kaise ho.";
+        System.out.println(reverseString(str));
+
+    }
+}
