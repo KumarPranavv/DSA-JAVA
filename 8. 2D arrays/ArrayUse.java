3ef6c8fe6acb73730367c55dc8ed5b40bad25790
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-02 23:39:21 +0530

    Added new file

--- 8. 2D arrays/ArrayUse.java
+++ 8. 2D arrays/ArrayUse.java
@@ -2,6 +2,8 @@ public class ArrayUse{
     public static void main(String[] args){
         int arr2d[][]=new int[3][3];
         System.out.println(arr2d[0][1]);
-        
+        arr2d[0][2]=54;
+        System.out.println(arr2d[0][2]);
+
     }
 }
