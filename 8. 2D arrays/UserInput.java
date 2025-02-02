7a84ca7c31dce33ac078345709e72c384c5bf593
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-02 23:51:26 +0530

    Added new file

---
+++ 8. 2D arrays/UserInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class UserInput{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows: ");
+        int rows=sc.nextInt();
+        System.out.println("Enter the no. of colms: ");
+        int colms=sc.nextInt();
+        int input[][]=new int[rows][colms];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<colms;j++){
+                System.out.println("Enter the element at "+i+" row "+j+" column ");
+                input[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<colms;j++){
+                System.out.print(input[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
