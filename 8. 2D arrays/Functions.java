61972c7f4cf6de0ab7479361ba6a102c5f4b3361
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-03 16:01:34 +0530

    Added new file

---
+++ 8. 2D arrays/Functions.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Functions{
+    public static int[][] takeInput(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no. of rows: ");
+        int rows=sc.nextInt();
+        System.out.println("Enter the no. of columns: ");
+        int cols=sc.nextInt();
+        int[][] input=new int[rows][cols]; 
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.println("Enter the element at "+i+" row "+j+" column ");
+                input[i][j]=sc.nextInt();
+            }
+        }
+        return input;
+
+    }
+    public static void print(int[][] input){
+        int rows=input.length;
+        int cols=input[0].length;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.print(input[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int arr2[][]={{1,2,3},{1,2,3}};
+        print(arr2);
+    }
+}
