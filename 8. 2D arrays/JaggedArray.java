20d5ca8346f320fbc0cf7577f7b63d792e04624a
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-06 09:14:54 +0530

    Updated a file

--- 8. 2D arrays/JaggedArray.java
+++ 8. 2D arrays/JaggedArray.java
@@ -3,5 +3,10 @@ public class JaggedArray{
     public static void main(String[] args){
         int arr[][]=new int[3][5];
         System.out.println(arr[0]);
+
+        int arr2[][]=new int[4][];// no. of columns not specified
+        System.out.println(arr2);
+        System.out.println(arr2[0]);
+
     }
 }
