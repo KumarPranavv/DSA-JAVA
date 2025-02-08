d40bada5005c4c3a4063b3719a51c7b39374923a
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-08 16:42:20 +0530

    Added new file

---
+++ 8. 2D arrays/Practice/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+public class RemoveDuplicates{
+    public static String remove(String s){
+        int n=s.length();
+        if(n==0){
+            return s;
+        }
+        String ans="";
+        int startIndex=0;
+        while(startIndex<n){
+            char uniqueChar=s.charAt(startIndex);
+            int nextUniq=startIndex+1;
+            while(nextUniq<n && s.charAt(nextUniq)==uniqueChar){
+                nextUniq+=1;
+
+            }
+            ans+=uniqueChar;
+            startIndex=nextUniq;
+        }
+        return ans;
+
+
+    }
+    public static void main(String[] args){
+        
+    }
+}
