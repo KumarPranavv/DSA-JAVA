28ee1e480f48bf66d8e26454ea309fe785c4fe89
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-12 20:06:30 +0530

    Added new file

---
+++ Practice/Sorting/Bubble.java
@@ -0,0 +1,17 @@
+public class Bubble{
+    public static void sort(int[] arr){
+        int n =arr.length;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
