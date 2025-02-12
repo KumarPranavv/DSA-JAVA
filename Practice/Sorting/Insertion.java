93d8570f55ad2520094b5c637c41b7ef40bfe822
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-12 20:01:30 +0530

    Added new file

--- Practice/Sorting/Insertion.java
+++ Practice/Sorting/Insertion.java
@@ -3,7 +3,7 @@ public class Insertion{
         int n=arr.length;
         for(int i=0;i<n;i++){
             int j=i;
-            while(j>=0 && arr[j-1]>arr[j]){
+            while(j>0 && arr[j-1]>arr[j]){
                 int temp=arr[j-1];
                 arr[j-1]=arr[j];
                 arr[j]=temp;
@@ -13,6 +13,8 @@ public class Insertion{
         }
     }
     public static void main(String[] args){
+        int arr[]={67,24,45,32,8,54};
+        sort(arr);
 
     }
 }
