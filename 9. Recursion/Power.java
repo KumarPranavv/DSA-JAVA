e43e424a25c98965dd7c15bac7ab318728e3fc96
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-18 23:02:50 +0530

    Added new file

---
+++ 9. Recursion/Power.java
@@ -0,0 +1,26 @@
+public class Power{
+    public static int pow(int x, int n){
+        if(x==0 && n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        int smallAns=pow(x,n/2);
+        if(n%2==0){
+            return smallAns*smallAns;
+        }
+        else{
+            return x*smallAns*smallAns;
+        }
+    }
+    public static void main(String[] args){
+        int x=4;
+        int n=3;
+        int ans=pow(x,n);
+        System.out.println(ans);
+    }
+}
