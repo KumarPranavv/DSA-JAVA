e98fd31fb7555821f4d2ec3a6571075ac4f87ffa
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-18 22:58:17 +0530

    Added new file

---
+++ 9. Recursion/SumOfNum.java
@@ -0,0 +1,13 @@
+public class SumOfNum{
+    public static int sum(int n){
+        if(n==1){
+            return 1;
+        }
+        return sum(n-1)+n;
+    }
+    public static void main(String[] args){
+        int n=10;
+        int ans= sum(n);
+        System.out.println(ans);
+    }
+}
