f4889a9e800a3e9b70716e27344527bf7fa830cd
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-19 23:31:38 +0530

    Updated a file

--- 9. Recursion/Factorial.java
+++ 9. Recursion/Factorial.java
@@ -7,7 +7,7 @@ public class Factorial{
         return n*smallAns;
     }
     public static void main(String[] args){
-        int n=6;
+        int n=7;
         int ans=fact(n);
         System.out.println(ans);
     }
