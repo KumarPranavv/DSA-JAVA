d05a7ca28c4ba009de24c4a6c74020add29e6edb
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-20 23:28:25 +0530

    Added new file

--- 9. Recursion/Digits.java
+++ 9. Recursion/Digits.java
@@ -7,7 +7,7 @@ public class Digits{
         return smallAns+1;
     }
     public static void main(String[] args){
-        int n =2390399;
+        int n =3299;
         System.out.println(count(n));
     }
 }
