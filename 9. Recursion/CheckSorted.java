97f59d4fe0f16e70a2eee49b97b7bed6ca3c7e01
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-02-23 20:23:38 +0530

    Added new file

---
+++ 9. Recursion/CheckSorted.java
@@ -0,0 +1,26 @@
+public class CheckSorted{
+    public static boolean check(int[] arr){
+        int n=arr.length;
+        if(n<=1){
+            return true;
+        }
+        int smallArr[]=new int[n-1];
+        for(int i=1;i<n;i++){
+            smallArr[i-1]=arr[i];
+        }
+        boolean ans=check(smallArr);
+        if(!ans){
+            return false;
+        }
+        if(arr[0]<=arr[1]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
