bb567ea06fd1ec3b66b4b9105db28c80a3bd6d30
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-06-06 18:09:34 +0530

    Added new folder and files

---
+++ 10. Linked List/ArrayList/Intro.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+public class Intro{
+    public static void main(String[] args){
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        System.out.println(arr.size());
+    } 
+}
