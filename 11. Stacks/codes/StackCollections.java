bfcdacab0fa8a9590ece205e5cab954ed260aea0
Pranav Kumar <kumar.pranv1234@gmail.com>
2025-06-12 01:58:12 +0530

    Added new file

--- 11. Stacks/codes/StackCollections.java
+++ 11. Stacks/codes/StackCollections.java
@@ -10,5 +10,7 @@ public class StackCollections{
         System.out.println(stack.size());
         System.out.println(stack.peek());
         System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
     }
 }
